e898bf9454bcd1c64cbcfa09224b38a40a4d55bf
Job Hernandez <hj93@protonmail.com>
2024-02-06 16:33:08 -0800

    add square matrix multiplication

--- matrix.cpp
+++ matrix.cpp
@@ -48,12 +48,26 @@ public:
 
     return result;
   }
+
+  Matrix operator*(const Matrix& other) const {
+    Matrix result(elements);
+
+    for (size_t i = 0; i < elements.size(); i++) {
+      for (size_t j = 0; j < elements.size(); j++) {
+	result.elements[i][j] = 0.0;
+	for (size_t k = 0; k < elements.size(); k++) {
+	  result.elements[i][j] = result.elements[i][j] + elements[i][k] * other.elements[k][j];
+	}
+      }
+    }
+    return result;
+  }
 };
 
 int main() {
-  std::vector<std::vector<double>> m1_elements = {{3.0,4.0,5.0}, {2.0, 5.0, 6.0}};
+  std::vector<std::vector<double>> m1_elements = {{3.0,4.0}, {2.0, 5.0}};
 
-  std::vector<std::vector<double>> m2_elements = {{4.0,5.0, 6.0}, {6.0, 7.0, 8.0}};
+  std::vector<std::vector<double>> m2_elements = {{4.0,5.0}, {6.0, 7.0}};
 
   Matrix m1(m1_elements);
 
@@ -63,8 +77,12 @@ int main() {
 
   Matrix m4 = m2 - m1;
 
+  Matrix m5 = m2 * m1;
+
   m3.print_matrix();
   std::cout << std::endl;
   m4.print_matrix();
+  std::cout << std::endl;
+  m5.print_matrix();
 }
   
