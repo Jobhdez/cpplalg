db5d7910eadfae5538bf552c74f0dec35193ccec
Job Hernandez <hj93@protonmail.com>
2024-02-06 16:19:48 -0800

    add matrix subtraction

--- matrix.cpp
+++ matrix.cpp
@@ -37,15 +37,34 @@ public:
 
     return result;
   }
+
+  Matrix operator-(const Matrix& other) const {
+    Matrix result(elements);
+    for (size_t i = 0; i < elements.size(); i++) {
+      for (size_t j = 0; j < elements[0].size(); j++) {
+	result.elements[i][j] = elements[i][j] - other.elements[i][j];
+      }
+    }
+
+    return result;
+  }
 };
 
 int main() {
   std::vector<std::vector<double>> m1_elements = {{3.0,4.0,5.0}, {2.0, 5.0, 6.0}};
 
+  std::vector<std::vector<double>> m2_elements = {{4.0,5.0, 6.0}, {6.0, 7.0, 8.0}};
+
   Matrix m1(m1_elements);
 
-  Matrix m2 = m1 + m1;
+  Matrix m2(m2_elements);
 
-  m2.print_matrix();
+  Matrix m3 = m1 + m1;
+
+  Matrix m4 = m2 - m1;
+
+  m3.print_matrix();
+  std::cout << std::endl;
+  m4.print_matrix();
 }
   
