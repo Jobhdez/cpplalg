7d22d305c9b3c137fdfc59391825a90203ba3f4e
Job Hernandez <hj93@protonmail.com>
2024-02-14 19:56:52 -0800

    add trace tests

--- matrix_tests.cpp
+++ matrix_tests.cpp
@@ -5,6 +5,8 @@
 std::vector<std::vector<double>> m1_elements = {{3.0,4.0}, {2.0, 5.0}};
 std::vector<std::vector<double>> m2_elements = {{4.0,5.0}, {6.0, 7.0}};
 std::vector<std::vector<double>> elements = {{2,3,4}, {5,6,7}};
+std::vector<std::vector<double>> elements2 = {{2,1,5},{2,3,4},{0,1,0}};
+Matrix m0(elements2);
 Matrix m1(m1_elements);
 Matrix m2(m2_elements);
 
@@ -23,7 +25,7 @@ std::vector<std::vector<double>> matrix_pow_elements = {{9.0,16.0}, {4.0, 25.0}}
 
 std::vector<std::vector<double>> matrix_mul_elements = {{22.0,41.0}, {32.0, 59.0}};
 std::vector<std::vector<double>> matrix_transpose = {{2,5},{3,6},{4,7}};
-
+double trace = 5.0;
 TEST_CASE( "Matrix algebra", "[matrices]" ) {
   REQUIRE(m3.getElements() == matrix_sum_elements);
   REQUIRE(m4.getElements() == matrix_sub_elements);
@@ -31,4 +33,5 @@ TEST_CASE( "Matrix algebra", "[matrices]" ) {
   REQUIRE(m6.getElements() == matrix_scalar_elements);
   REQUIRE(m7.getElements() == matrix_pow_elements);
   REQUIRE(m9.getElements() == matrix_transpose);
+  REQUIRE(m0.trace() == trace);
 }
