91ae690893653a4f0ed722abfb95c609253be845
Job Hernandez <hj93@protonmail.com>
2024-02-14 18:58:37 -0800

    add transpose test

--- matrix_tests.cpp
+++ matrix_tests.cpp
@@ -4,6 +4,7 @@
 
 std::vector<std::vector<double>> m1_elements = {{3.0,4.0}, {2.0, 5.0}};
 std::vector<std::vector<double>> m2_elements = {{4.0,5.0}, {6.0, 7.0}};
+std::vector<std::vector<double>> elements = {{2,3,4}, {5,6,7}};
 Matrix m1(m1_elements);
 Matrix m2(m2_elements);
 
@@ -12,6 +13,8 @@ Matrix m4 = m2 - m1;
 Matrix m5 = m2 * m1;
 Matrix m6 = m1.mulByScalar(2.0);
 Matrix m7 = m1.power(2.0);
+Matrix m8(elements);
+Matrix m9 = m8.transpose();
 
 std::vector<std::vector<double>> matrix_sum_elements = {{7.0,9.0}, {8.0, 12.0}};
 std::vector<std::vector<double>> matrix_sub_elements = {{1.0,1.0}, {4.0, 2.0}};
@@ -19,6 +22,7 @@ std::vector<std::vector<double>> matrix_scalar_elements = {{6.0,8.0}, {4.0, 10.0
 std::vector<std::vector<double>> matrix_pow_elements = {{9.0,16.0}, {4.0, 25.0}};
 
 std::vector<std::vector<double>> matrix_mul_elements = {{22.0,41.0}, {32.0, 59.0}};
+std::vector<std::vector<double>> matrix_transpose = {{2,5},{3,6},{4,7}};
 
 TEST_CASE( "Matrix algebra", "[matrices]" ) {
   REQUIRE(m3.getElements() == matrix_sum_elements);
@@ -26,4 +30,5 @@ TEST_CASE( "Matrix algebra", "[matrices]" ) {
   REQUIRE(m5.getElements() == matrix_mul_elements);
   REQUIRE(m6.getElements() == matrix_scalar_elements);
   REQUIRE(m7.getElements() == matrix_pow_elements);
+  REQUIRE(m9.getElements() == matrix_transpose);
 }
