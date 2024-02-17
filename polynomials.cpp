2d028ef27525a7bbddffa4d96e79fb1fd3739cf8
Job Hernandez Lara <hj93@protonmail.com>
2024-02-16 23:12:53 -0800

    add polynomial addition

---
+++ polynomials.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+class Polynomial {
+private:
+  std::vector<double> coefficients;
+public:
+  Polynomial() {}
+
+  Polynomial(const std::vector<double>& coeffs) : coefficients(coeffs) {}
+
+  Polynomial(const Polynomial& other) : coefficients(other.coefficients) {}
+
+  ~Polynomial() {}
+
+  void print_poly() {
+    int exponent = coefficients.size();
+    std::cout << "+"  << std::endl;
+    for (size_t i = 0; i<coefficients.size(); i++) {
+      std::cout << coefficients[i] << "x^" << exponent << " " << std::endl;
+      exponent -= 1;
+    }
+  }
+
+  Polynomial operator+(Polynomial& other) const {
+    Polynomial poly(coefficients);
+    for (size_t i = 0; i < coefficients.size(); i++) {
+      poly.coefficients[i] = coefficients[i] + other.coefficients[i];
+    }
+    return poly;
+  }
+};
+
+int main() {
+  std::vector<double> coeffs = {3.0, 2.0, 4.0};
+  Polynomial poly(coeffs);
+  Polynomial poly2 = poly + poly;
+  poly2.print_poly();
+}
+  
