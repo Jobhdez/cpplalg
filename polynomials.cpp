65a49d6b56d869b8d3b2423beaceadd4d7842163
Job Hernandez <hj93@protonmail.com>
2024-02-19 16:53:36 -0800

    add poly multiplication

--- polynomials.cpp
+++ polynomials.cpp
@@ -14,7 +14,7 @@ public:
   ~Polynomial() {}
 
   void print_poly() {
-    int exponent = coefficients.size();
+    int exponent = coefficients.size() - 1;
     std::cout << "+"  << std::endl;
     for (size_t i = 0; i<coefficients.size(); i++) {
       std::cout << coefficients[i] << "x^" << exponent << " " << std::endl;
@@ -36,12 +36,24 @@ public:
     }
     return poly;
   }
+
+  Polynomial operator*(Polynomial& other) const {
+    double degreep1 = coefficients.size();
+    double degreep2 = other.coefficients.size();
+    Polynomial result(std::vector<double>(degreep1 + degreep2 - 1));
+    for (size_t i = 0; i < degreep1; i++) {
+      for (size_t j = 0; j < degreep2; j++) {
+	result.coefficients[i+j] += coefficients[i] * other.coefficients[j];
+	  }
+    }
+    return result;
+  }
 };
 
 int main() {
   std::vector<double> coeffs = {3.0, 2.0, 4.0};
   Polynomial poly(coeffs);
-  Polynomial poly2 = poly + poly;
+  Polynomial poly2 = poly * poly;
   poly2.print_poly();
 }
   
