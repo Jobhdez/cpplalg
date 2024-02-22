441f8dbfa576a6d430cb908a670630750132ab81
Job Hernandez <hj93@protonmail.com>
2024-02-21 22:25:23 -0800

    add matrix-vec multiplication

--- matrix_tests.cpp
+++ matrix_tests.cpp
@@ -6,9 +6,11 @@ std::vector<std::vector<double>> m1_elements = {{3.0,4.0}, {2.0, 5.0}};
 std::vector<std::vector<double>> m2_elements = {{4.0,5.0}, {6.0, 7.0}};
 std::vector<std::vector<double>> elements = {{2,3,4}, {5,6,7}};
 std::vector<std::vector<double>> elements2 = {{2,1,5},{2,3,4},{0,1,0}};
+std::vector<std::vector<double>> elements3 = {{1,-1,2},{0,-3,1}};
 Matrix m0(elements2);
 Matrix m1(m1_elements);
 Matrix m2(m2_elements);
+Matrix mat3(elements3);
 
 Matrix m3 = m1 + m2;
 Matrix m4 = m2 - m1;
@@ -17,7 +19,9 @@ Matrix m6 = m1.mulByScalar(2.0);
 Matrix m7 = m1.power(2.0);
 Matrix m8(elements);
 Matrix m9 = m8.transpose();
-
+std::vector<double> vec = {2,1,0};
+std::vector<double> vec2 = {1, -3};
+std::vector<double> v = mat3.mulByVector(vec);
 std::vector<std::vector<double>> matrix_sum_elements = {{7.0,9.0}, {8.0, 12.0}};
 std::vector<std::vector<double>> matrix_sub_elements = {{1.0,1.0}, {4.0, 2.0}};
 std::vector<std::vector<double>> matrix_scalar_elements = {{6.0,8.0}, {4.0, 10.0}};
@@ -34,4 +38,5 @@ TEST_CASE( "Matrix algebra", "[matrices]" ) {
   REQUIRE(m7.getElements() == matrix_pow_elements);
   REQUIRE(m9.getElements() == matrix_transpose);
   REQUIRE(m0.trace() == trace);
+  REQUIRE(v == vec2);
 }
