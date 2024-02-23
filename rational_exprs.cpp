d5e37937d962ec1593c75889591efdeea471dd09
Job Hernandez <hj93@protonmail.com>
2024-02-22 19:45:12 -0800

    add addition

---
+++ rational_exprs.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "polynomials.cpp"
+
+class RationalExp {
+private:
+  Polynomial numerator;
+  Polynomial denominator;
+
+public:
+  RationalExp() {}
+  RationalExp(const RationalExp& other) : numerator(other.numerator), denominator(other.denominator) {}
+  RationalExp(const Polynomial& p1,const  Polynomial& p2) : numerator(p1), denominator(p2) {}
+  ~RationalExp() {}
+
+  void print_rational_exp() {
+    numerator.print_poly();
+    std::cout << "/" <<std::endl;
+    denominator.print_poly();
+  }
+
+  RationalExp operator+(RationalExp& other) const {
+    Polynomial numerRat1 = numerator * other.denominator;
+    Polynomial denomRat1 = denominator * other.denominator;
+
+    Polynomial numerRat2 = other.numerator * denominator;
+    Polynomial denomRat2 = other.denominator * denominator;
+
+    Polynomial sumRat1 = numerRat1 + numerRat2;
+    Polynomial sumRat2 = denomRat1 + denomRat2;
+
+    return RationalExp(sumRat1, sumRat2);
+  }
+};
+
+int main() {
+  std::vector<double> el = {2.0, 3.0};
+  Polynomial p1(el);
+
+  RationalExp rat1(p1, p1);
+  RationalExp r(rat1);
+  RationalExp rat2 = rat1 + rat1;
+  rat2.print_rational_exp();
+
+  return 0;
+}
