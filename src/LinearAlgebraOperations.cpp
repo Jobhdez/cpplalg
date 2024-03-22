e03f150a4659ba0c716367eecab1cb88da10fdfb
Job Hernandez <hj93@protonmail.com>
2024-03-22 16:04:10 -0700

    run clang formatter

--- src/LinearAlgebraOperations.cpp
+++ src/LinearAlgebraOperations.cpp
@@ -7,50 +7,52 @@ using namespace std;
 namespace LinearAlgebra {
 namespace Matrix {
 
-  template<typename T>
-  vector<vector<T>> add(const vector<vector<T>>& matrix1, const vector<vector<T>>& matrix2) {
-    assert(matrix1.size() == matrix2.size());
-    assert(matrix1[0].size() == matrix2[0].size());
-    vector<vector<T>> result_matrix(matrix1.size(), vector<T>(matrix1[0].size()));
+template <typename T>
+vector<vector<T>> add(const vector<vector<T>> &matrix1,
+                      const vector<vector<T>> &matrix2) {
+  assert(matrix1.size() == matrix2.size());
+  assert(matrix1[0].size() == matrix2[0].size());
+  vector<vector<T>> result_matrix(matrix1.size(), vector<T>(matrix1[0].size()));
 
-    for (size_t i = 0; i < matrix1.size(); i++) {
-        for (size_t j = 0; j < matrix1[0].size(); j++) {
-            result_matrix[i][j] = matrix1[i][j] + matrix2[i][j];
-        }
+  for (size_t i = 0; i < matrix1.size(); i++) {
+    for (size_t j = 0; j < matrix1[0].size(); j++) {
+      result_matrix[i][j] = matrix1[i][j] + matrix2[i][j];
     }
+  }
 
-    return result_matrix;
+  return result_matrix;
 }
-  template<typename T>
-  vector<vector<T>> sub(const vector<vector<T>>& matrix1, const vector<vector<T>>& matrix2) {
-    assert(matrix1.size() == matrix2.size());
-    assert(matrix1[0].size() == matrix2[0].size());
-    vector<vector<T>> result_matrix(matrix1.size(), vector<T>(matrix1[0].size()));
+template <typename T>
+vector<vector<T>> sub(const vector<vector<T>> &matrix1,
+                      const vector<vector<T>> &matrix2) {
+  assert(matrix1.size() == matrix2.size());
+  assert(matrix1[0].size() == matrix2[0].size());
+  vector<vector<T>> result_matrix(matrix1.size(), vector<T>(matrix1[0].size()));
 
-    for (size_t i = 0; i < matrix1.size(); i++) {
-        for (size_t j = 0; j < matrix1[0].size(); j++) {
-            result_matrix[i][j] = matrix1[i][j] - matrix2[i][j];
-        }
+  for (size_t i = 0; i < matrix1.size(); i++) {
+    for (size_t j = 0; j < matrix1[0].size(); j++) {
+      result_matrix[i][j] = matrix1[i][j] - matrix2[i][j];
     }
+  }
 
-    return result_matrix;
+  return result_matrix;
 }
-  template<typename T>
-  vector<vector<T>> mul(const vector<vector<T>>& matrix1, const vector<vector<T>>& matrix2) {
-    assert(matrix1[0].size() == matrix2.size());
-    vector<vector<T>> result_matrix(matrix1.size(), vector<T>(matrix2[0].size()));
+template <typename T>
+vector<vector<T>> mul(const vector<vector<T>> &matrix1,
+                      const vector<vector<T>> &matrix2) {
+  assert(matrix1[0].size() == matrix2.size());
+  vector<vector<T>> result_matrix(matrix1.size(), vector<T>(matrix2[0].size()));
 
-    for (size_t i = 0; i < matrix1.size(); i++) {
-        for (size_t j = 0; j < matrix2[0].size(); j++) {
-            result_matrix[i][j] = 0.0;
-            for (size_t k = 0; k < matrix1[0].size(); k++) {
-                result_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
-            }
-        }
+  for (size_t i = 0; i < matrix1.size(); i++) {
+    for (size_t j = 0; j < matrix2[0].size(); j++) {
+      result_matrix[i][j] = 0.0;
+      for (size_t k = 0; k < matrix1[0].size(); k++) {
+        result_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
+      }
     }
-    return result_matrix;
+  }
+  return result_matrix;
 }
-
 
   template<typename T>
   void print(const vector<vector<T>>& elements) {
